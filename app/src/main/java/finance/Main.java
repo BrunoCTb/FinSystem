584063a332b13a7fc2f72eac7a593fd1fedf248f
BrunoCTmb <zazaduza10@gmail.com>
2024-09-24 17:06:24 -0300

    create dynamic script sql by list of map of strings and createTable method

--- app/src/main/java/finance/Main.java
+++ app/src/main/java/finance/Main.java
@@ -2,9 +2,26 @@ package finance;
 
 import finance.sqlite.Sqlite;
 
+import java.sql.Statement;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         Sqlite sqlite = new Sqlite();
-        sqlite.connection();
+
+        // [ ["id", "INTEGER PRIMARY KEY"], ["title", "VARCHAR(250)"], [] ]
+        List<Map<String, String>> sqlValues = new ArrayList<>();
+
+        // List<Map<String, String>>
+        sqlValues.add(Map.of("id", "INTEGER PRIMARY KEY"));
+        sqlValues.add(Map.of("title", "VARCHAR(250)"));
+
+        String tableValues = sqlite.makeSqlSript(sqlValues);
+
+        sqlite.createTable("tabela_test", tableValues);
+
+
     }
 }
--- app/src/main/java/finance/sqlite/Sqlite.java
+++ app/src/main/java/finance/sqlite/Sqlite.java
@@ -3,18 +3,57 @@ package finance.sqlite;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
 
 public class Sqlite {
 
-    public void connection() {
-        String dbRelativePath = "src/main/resources/db/";
-        String dbName = "finance.db";
+    private final String DB_RELATIVE_PATH = "src/main/resources/db/";
+    private final String DB_NAME = "finance.db";
 
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbRelativePath + dbName)) {
-            System.out.println("Conexao com sucesso");
+    public Statement connection() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DB_RELATIVE_PATH + DB_NAME);
+            Statement statement = connection.createStatement();
+
+            return statement;
+        } catch (SQLException e) {
+            System.out.println("Exception ao gerar conexão: " + e);
+            return null;
+        }
+    }
+
+    public void createTable(String tableName, String tableValues){
+        String script = "CREATE TABLE " + tableName + "(\n" +
+                tableValues +
+                " \n);";
+
+        try (Statement statement  = this.connection()) {
+            statement.execute(script);
+
+            System.out.println("A tabela " + tableName + " foi criada com sucesso!");
         } catch (SQLException e) {
             System.out.println("Exception: " + e);
         }
+    }
+
+    public String makeSqlSript(List<Map<String, String>> sqlValues) {
+        String script = "";
+        String vSyntax = ","; // virgula que vai ser removida caso seja a ultima linha
+        int loop = sqlValues.size() - 1; // contaagem do loop
+
+        for (Map<String, String> map : sqlValues) {
+            for (Map.Entry<String, String> i : map.entrySet()) {
+                if (loop <= 0) {
+                    vSyntax = "";
+                }
+                script += "    " + i.getKey() + " " + i.getValue() + vSyntax + "\n";
+                loop --;
+            }
+        }
+
+        return script;
     }
 
 }
